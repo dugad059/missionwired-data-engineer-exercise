e7ad1de21e072d75deb15a54f8589be2f9d24fb6
David Dugas <daviddugas@Davids-MacBook-Pro.local>
2022-09-01 14:10:05 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# MissionWired Data Engineer Exercise
+This repository contains my code and csv output files for MissionWired's data engineering hiring exercise. For ease of use, I have formated my code in both a Python file and a Jupyter Notebook! The Python file is intended for those who want to generate the people.csv and acquisition_facts.csv quickly. Jupyter Notebooks are intended to understand my thought process in how I created the csv files. On the Jupyter Notebooks, there are comprehensive notes and comments on what/why things are being done at each step. I provide examples of how the data looks at each step of transformation. 
+
+## The Repository is structured as follows:
+
+Python File
+- [exercise_1_2.py]() - Code for Exercise 1 & 2
+
+Jupyter Notebooks
+- [exercise_1.ipynb]() - Code for Exercise 1
+- [exercise_2.ipynb]() - Code for Exercise 2
+
+Output Files
+- [people.csv]() - Output CSV from Exercise 1
+- [acquisition_facts.csv]() - Output CSV from Exercise 2
+
+## How to run locally
+The repository can be cloned in order to run the file and notebooks on your local machine.
+#### Dependencies
+- [Jupyter](https://jupyter.org/install)
+- [Pandas](https://pandas.pydata.org/docs/getting_started/install.html) 
+#### Running Python File
+- In you command line, run 'python exercise_1_2.py'
+#### Running Jupyter Notebook
+- Open [exercise_1.ipynb]() and run blocks in order.
+- Once the people.cvs file is save from [exercise_1.ipynb](), Open [exercise_2.ipynb]() and run blocks in order! 
+
---
+++ exercise_1_2.py
@@ -0,0 +1,70 @@
+import pandas as pd
+
+# EXERCISE 1
+
+print('Loading data exercise 1...')
+# Cons Table
+cons = pd.read_csv('https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons.csv')
+
+# Emails Table
+cons_email = pd.read_csv('https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons_email.csv')
+
+# Subs Table
+subs = pd.read_csv('https://als-hiring.s3.amazonaws.com/fake_data/2020-07-01_17%3A11%3A00/cons_email_chapter_subscription.csv')
+print('Loading data exercise 1 COMPLETE...')
+
+print('Merging / Transforming data exercise 1...')
+# Merging cons table with cons_email table on cons_id
+merge_1 = pd.merge(
+    left=cons[['cons_id', 'source', 'create_dt', 'modified_dt']],
+    right=cons_email[cons_email['is_primary'] == 1][['cons_email_id', 'cons_id', 'email', 'is_primary']],
+    on='cons_id',
+    how='left'
+)
+
+# Merging merge_1 to subs table on cons_email_id
+merge_2 = pd.merge(
+    left=merge_1,
+    right=subs[subs['chapter_id'] == 1][['cons_email_id', 'chapter_id', 'isunsub']],
+    on='cons_email_id',
+    how='left'
+).astype({'isunsub': bool}) # Changes isunsub from int to bool
+
+# Imports only columns needed 
+merge_2 = merge_2[['email', 'source', 'isunsub', 'create_dt', 'modified_dt']]
+
+# Changes dtypes from object to datetime
+merge_2['create_dt'] = pd.to_datetime(merge_2['create_dt'], format='%a, %Y-%m-%d %H:%M:%S')
+merge_2['modified_dt'] = pd.to_datetime(merge_2['modified_dt'], format='%a, %Y-%m-%d %H:%M:%S')
+
+# Renames columns to naming convention per schema
+merge_2.columns=['email', 'code', 'is_unsub', 'created_dt', 'updated_dt']
+
+print('Merging / Transforming data exercise 1... COMPLETE')
+
+# Converting to csv
+merge_2.to_csv('people.csv')
+print('people.csv SAVED')
+
+# --------------------------------------------------------------
+
+# EXERCISE 2
+
+print('Loading data exercise 2...')
+# Imported csv from exercise 1
+people = pd.read_csv('people.csv', usecols=['email', 'created_dt'], parse_dates=['created_dt'])
+print('Loading data exercise 2...COMPLETE')
+
+print('Merging / Transforming data exercise 2...')
+# Renames columns to naming convention per schema
+people.columns=['email', 'acquisition_date']
+#Change dtype from datetime to date
+people['acquisition_date'] = people['acquisition_date'].dt.date
+
+# A groupby to count the constituents acquired on specific date. Renamed the resulted aggregate column to acquisitions.
+acquisitions = people.groupby('acquisition_date')['email'].count().rename('acquisitions')
+print('Merging / Transforming data exercise 2... COMPLETE')
+
+# Converting to csv
+acquisitions.to_csv('acquisition_facts.csv')
+print('acquisition_facts.csv SAVED')
